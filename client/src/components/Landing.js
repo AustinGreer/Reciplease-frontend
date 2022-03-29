d79b74035c22fba5c9966966d79be50aa6140eb3
Austin Greer <agreer.ag12@gmail.com>
2022-03-28 23:23:10 -0400

    added antd button to landing and made style adjustments to landing with mobile first design

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button } from 'antd';
+
 import '../styles/landing.scss'
 
 function Landing() {
     const history = useNavigate();
 
-    const onSubmit = () => {
+    const handleClick = () => {
         history('/login');
     }
-    
+
     return (
         <div className="content">
             <h1 className="landing-title">Reciplease</h1>
             <p className="landing-description">Don't know what to cook next?</p>
-            <button onClick={onSubmit} id='landing-btn'>Let's get cook'n</button>
+            <div className='button-container'>
+                <Button type="primary" onClick={handleClick}>Let's get cook'n</Button>
+            </div>
         </div>
     )
 }
