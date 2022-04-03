714ef27a5bf5f2ea524b79b0dcbfc46e3e696be7
Austin Greer <agreer.ag12@gmail.com>
2022-04-03 19:00:27 -0400

    made adjustments to landing button

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -15,9 +15,7 @@ function Landing() {
         <div className="content">
             <h1 className="landing-title">Reciplease</h1>
             <p className="landing-description">Don't know what to cook next?</p>
-            <div className='button-container'>
-                <Button type="primary" onClick={handleClick}>Let's get cook'n</Button>
-            </div>
+            <Button type="primary" className='landing-btn' onClick={handleClick}>Let's get cook'n</Button>
         </div>
     )
 }
