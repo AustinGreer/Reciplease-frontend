0f2cb958743d1d29351f7eb8ff2245c2cd597d1b
Austin Greer <agreer.ag12@gmail.com>
2022-04-26 21:47:34 -0400

    signup redesign wip

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { Loading3QuartersOutlined } from '@ant-design/icons';
+import { Input, Button, Alert } from 'antd';
+import {useNavigate, Link} from 'react-router-dom';
+import axios from 'axios';
+
 import * as yup from 'yup';
 import schema from '../formSchema/signSchema';
-import axios from 'axios';
-import {useNavigate, Link} from 'react-router-dom';
-import { Loading3QuartersOutlined } from '@ant-design/icons'
 
 import "../styles/signup.scss";
 import { getUser } from '../actions';
-import { useDispatch } from 'react-redux';
 
 
 
@@ -21,12 +22,6 @@ function SignUp() {
     const [formValues, setFormValues] = useState(initialValues)
     const [formErrors, setFormErrors] = useState(initialValues)
     const [disabledBtn, setDisabledBtn] = useState(true)
-    const [signUpSuccess, setSignUpSuccess] = useState({
-        message: "",
-        activeClass: ""
-    })
-
-    const dispatch = useDispatch();
     
     const push = useNavigate();
 
@@ -61,80 +56,66 @@ function SignUp() {
     }, [formValues])
 
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        setSignUpSuccess({
-            message: <Loading3QuartersOutlined spin style={loadingIconStyle} />,
-            activeClass: signUpSuccess.activeClass
-        })
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
         
-        axios.post('https://reciplease-backend.vercel.app/users/register', formValues)
-            .then((res) => {
-                setSignUpSuccess({
-                    message: "Success!",
-                    activeClass:"success-modal"
-                })
-                dispatch(getUser({username: formValues.username, email: formValues.email}));
-                localStorage.setItem('user', JSON.stringify({username: formValues.username, email: formValues.email}))
-                localStorage.setItem("token", JSON.stringify(res.data.token))
-                setTimeout(() => {
-                    push("/search")
-                }, 1500)
-            })
-            .catch(err => {
-                setSignUpSuccess({
-                    message: err.response.data.message,
-                    activeClass: "error-modal"
-                })
+    //     axios.post('https://reciplease-backend.vercel.app/users/register', formValues)
+    //         .then((res) => {
+    //             dispatch(getUser({username: formValues.username, email: formValues.email}));
+    //             localStorage.setItem('user', JSON.stringify({username: formValues.username, email: formValues.email}))
+    //             localStorage.setItem("token", JSON.stringify(res.data.token))
+    //             setTimeout(() => {
+    //                 push("/search")
+    //             }, 1500)
+    //         })
+    //         .catch(err => {
 
-                console.log({err})
-            })
-            .finally(() => {
-                setFormValues(initialValues)
-            })
-    }
-
-    const loadingIconStyle = {
-        fontSize: '3rem',
-        width: '100%'
-    }
+    //             console.log({err})
+    //         })
+    //         .finally(() => {
+    //             setFormValues(initialValues)
+    //         })
+    // }
 
 
     return (
-        <div className="signup-container">
-                <form onSubmit={handleSubmit}>
-                    <h3>Sign Up</h3>
-                    <p>Sign up today and begin searching for your favorite recipes right away!</p>
+        <div className="signup-screen">
+            <div className="signup-container">
+                <h1>Sign Up</h1>
+                <p className='signup-desc'>Sign up today and begin searching for your favorite recipes right away!</p>
 
-                    { signUpSuccess ? <p className={signUpSuccess.activeClass}>{signUpSuccess.message}</p> : null}
+                <form>
+                    <div className="input-container">
+                        <Input
+                            className="signup-input"
+                            variant="outlined"
+                            name='username'
+                            type='text'
+                            value={formValues.username}
+                            onChange={handleInputChange}
+                            placeholder='Username'
+                        /> 
 
-                    { formErrors.username && 
-                        <p className="errors">{formErrors.username}</p> }
-                    <input
-                        className="signup-input"
-                        variant="outlined"
-                        name='username'
-                        type='text'
-                        value={formValues.username}
-                        onChange={handleInputChange}
-                        placeholder='Username'
-                        />
+                        { formErrors.username && <p className='errors'>{formErrors.username}</p>}
 
-                    { formErrors.email && 
-                        <p className="errors">{formErrors.email}</p> }
-                    <input
+                    </div>
+
+                    <div className="input-container">
+                    <Input
                         className="signup-input"
                         name='email'
                         type='email'
                         value={formValues.email}
                         onChange={handleInputChange}
                         placeholder='Email'
-                    />
+                    /> 
 
-                    { formErrors.password && 
-                        <p className="errors">{formErrors.password}</p> }
-                    <input
+                        { formErrors.email && <p className='errors'>{formErrors.email}</p>}
+
+                    </div>
+
+                    <div className="input-container">
+                    <Input
                         className="signup-input"
                         name='password'
                         type='password'
@@ -142,13 +123,20 @@ function SignUp() {
                         onChange={handleInputChange}
                         placeholder='Password'
                     />
-                        
-                        <button disabled={disabledBtn} type="submit" className="signup-btn">Let's get cook'n</button>
 
-                    <p className='options'>
-                        Already have an account? <Link to="/login">Login here</Link>
-                    </p>
+                        { formErrors.password && <p className='errors'>{formErrors.password}</p>}
+
+                    </div>
+
+                    <Button htmlType="submit" type="primary" className="signup-btn">Let's get cook'n</Button>
+
                 </form>
+                    <p className='options'>
+                        Already have an account? <br /> 
+                        <Link to="/login">Login here</Link>
+                    </p>
+
+            </div>
             </div>
     )
 }
